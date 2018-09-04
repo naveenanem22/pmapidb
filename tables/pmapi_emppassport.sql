0b62c497e4c308fbd55bb35ea7078d9615a2eadc
Naveen Anem <KH2475@kitspl.com>
2018-09-04 21:48:46 +0530

    Updating fk for emppassport

--- tables/pmapi_emppassport.sql
+++ tables/pmapi_emppassport.sql
@@ -4,5 +4,6 @@ CREATE TABLE `emppassport` (
   `epp_dateofissue` date DEFAULT NULL,
   `epp_dateofexpiry` date DEFAULT NULL,
   `epp_empid` varchar(45) NOT NULL,
-  PRIMARY KEY (`epp_empid`)
+  PRIMARY KEY (`epp_empid`),
+  CONSTRAINT `fk_epp_empid_emp_id` FOREIGN KEY (`epp_empid`) REFERENCES `employee` (`emp_id`)
 );
