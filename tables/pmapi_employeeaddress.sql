1d09b58699c1118cdd3bdd4790ca669c0856827a
Naveen Anem <KH2475@kitspl.com>
2018-11-05 13:02:27 +0530

    Modifying address table anme

--- tables/pmapi_employeeaddress.sql
+++ tables/pmapi_employeeaddress.sql
@@ -1,11 +1,11 @@
 DROP TABLE IF EXISTS employeeaddress;
 CREATE TABLE employeeaddress (
     empaddr_id INT(6) NOT NULL UNIQUE,
-    empaddr_paddr_id INT(6) NOT NULL,
+    empaddr_ia_id INT(6) NOT NULL,
 	empaddr_emp_id INT(6) NOT NULL,	
     CONSTRAINT pk_employeeaddress PRIMARY KEY (empaddr_id),
-    CONSTRAINT fk_empaddr_paddr_id_ref_paddr_id FOREIGN KEY (empaddr_paddr_id)
-        REFERENCES personaladdress (paddr_id),
+    CONSTRAINT fk_empaddr_paddr_id_ref_paddr_id FOREIGN KEY (empaddr_ia_id)
+        REFERENCES individualaddress (ia_id),
 	CONSTRAINT fk_empaddr_emp_id_ref_emp_id FOREIGN KEY (empaddr_emp_id)
 		REFERENCES employee (emp_id)
 );
---
+++ tables/pmapi_individualaddress.sql
@@ -0,0 +1,15 @@
+DROP TABLE IF EXISTS individualaddress;
+CREATE TABLE individualaddress (
+    ia_id INT(6) NOT NULL UNIQUE,
+    ia_street_name VARCHAR(45) NOT NULL,
+    ia_building_number VARCHAR(45),
+    ia_city VARCHAR(45),
+    ia_state VARCHAR(45),
+    ia_country VARCHAR(45),
+    ia_postal_code INT(10),
+    ia_addr_line_1 VARCHAR(120),
+    ia_addr_line_2 VARCHAR(120),
+    ia_addr_line_3 VARCHAR(120),
+    ia_addr_type VARCHAR(45),
+    CONSTRAINT pk_individualaddress PRIMARY KEY (ia_id)
+);
