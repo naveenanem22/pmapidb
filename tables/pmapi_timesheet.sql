22957d418d8aedaad75e805fc392094dee132b66
Naveen Anem <KH2475@kitspl.com>
2018-11-08 16:10:33 +0530

    fixing schema for Timesheets

--- tables/pmapi_timesheet.sql
+++ tables/pmapi_timesheet.sql
@@ -1,8 +1,12 @@
 DROP TABLE IF EXISTS timesheet;
 CREATE TABLE timesheet (
-	tmsht_id INT(6) NOT NULL UNIQUE AUTO_INCREMENT,	
-	tmsht_emp_id INT(6) NOT NULL,
-	tmsht_payroll_cycle_id INT(6) NOT NULL,
-	CONSTRAINT pk_timesheet PRIMARY KEY (tmsht_id),
-	CONSTRAINT fk_tmsht_emp_id_ref_emp_id FOREIGN KEY (tmsht_emp_id) REFERENCES employee(emp_id)
+	tmesht_id INT(6) NOT NULL UNIQUE AUTO_INCREMENT,
+	tmesht_tsk_id INT(6) NOT NULL,
+	tmesht_emp_id INT(6) NOT NULL,
+	tmesht_date DATE NOT NULL,
+	tmesht_start_time TIME NOT NULL,
+	tmesht_end_time TIME NOT NULL,
+	CONSTRAINT pk_worksegment PRIMARY KEY (tmesht_id),
+	CONSTRAINT fk_tmesht_emp_id_ref_emp_id FOREIGN KEY (tmesht_emp_id) REFERENCES employee(emp_id),
+	CONSTRAINT fk_tmesht_tsk_id_ref_tsk_id FOREIGN KEY (tmesht_tsk_id) REFERENCES task(tsk_id)	
 );
