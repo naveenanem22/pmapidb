8637753ae8e5c56998cb43587897a23607897ee8
Naveen Anem <KH2475@kitspl.com>
2018-11-16 12:49:16 +0530

    Update new cols in task

--- tables/pmapi_task.sql
+++ tables/pmapi_task.sql
@@ -4,6 +4,8 @@ CREATE TABLE task (
 	tsk_prj_id INT(6) NOT NULL,
 	tsk_name VARCHAR(100) NOT NULL,
 	tsk_description VARCHAR(2000),
+	tsk_start_date DATE NOT NULL,
+	tsk_end_date DATE NOT NULL,
 	CONSTRAINT pk_task PRIMARY KEY (tsk_id),
 	CONSTRAINT fk_tsk_prj_id_ref_prj_id FOREIGN KEY (tsk_prj_id) REFERENCES project(prj_id)
 );
