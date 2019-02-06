b02116c04c47bf026106b0ba75c178a819854d72
Naveen Anem <KH2475@kitspl.com>
2019-02-06 16:07:00 +0530

    AI for dept id

--- tables/pmapi_department.sql
+++ tables/pmapi_department.sql
@@ -1,6 +1,6 @@
 DROP TABLE IF EXISTS department;
 CREATE TABLE department (
-  dept_id INT(6) NOT NULL UNIQUE,
+  dept_id INT(6) NOT NULL UNIQUE AUTO_INCREMENT,
   dept_name VARCHAR(75) NOT NULL UNIQUE,
   CONSTRAINT pk_department PRIMARY KEY(dept_id)
 );
