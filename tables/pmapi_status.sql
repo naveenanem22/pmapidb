2aef0508454460cc28cc85e8cc3d9978f6a758c6
Naveen Anem <KH2475@kitspl.com>
2019-02-12 07:42:51 +0530

    Ticket related tables updated with latest stable changes

--- tables/pmapi_status.sql
+++ tables/pmapi_status.sql
@@ -1,6 +1,6 @@
 DROP TABLE IF EXISTS status;
 CREATE TABLE status (
-  sts_id INT(6) NOT NULL UNIQUE,
+  sts_id INT(6) NOT NULL UNIQUE AUTO_INCREMENT,
   sts_name VARCHAR(75) NOT NULL UNIQUE,
   sts_category VARCHAR(75) NOT NULL,
   CONSTRAINT pk_status PRIMARY KEY(sts_id)
--- tables/pmapi_tickettype.sql
+++ tables/pmapi_tickettype.sql
@@ -1,6 +1,6 @@
 DROP TABLE IF EXISTS tickettype;
 CREATE TABLE tickettype (
-  tkttype_id INT(6) NOT NULL UNIQUE,
+  tkttype_id INT(6) NOT NULL UNIQUE AUTO_INCREMENT,
   tkttype_name VARCHAR(75) NOT NULL UNIQUE,
   CONSTRAINT pk_tickettype PRIMARY KEY(tkttype_id)
 );
