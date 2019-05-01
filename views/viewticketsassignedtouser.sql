6d4fcab18bcdeeedda539d3a45c9f51b1594234d
Naveen Anem <KH2475@kitspl.com>
2019-05-01 13:03:59 +0530

    updated VIEW

--- views/viewticketsassignedtouser.sql
+++ views/viewticketsassignedtouser.sql
@@ -1,6 +1,7 @@
 DROP VIEW IF EXISTS viewticketsassignedtouser;
 CREATE VIEW viewticketsassignedtouser AS
-SELECT ticket.*, C.* FROM
+SELECT ticket.tkt_id, ticket.tkt_title, 
+ticket.tkt_updated_date, sts_name, pty_name, C.* FROM
 ticket
 INNER JOIN
 (SELECT A.*, B.latest_assigned_date FROM 
@@ -12,4 +13,7 @@ GROUP BY ta_tkt_id) B
 ON A.ta_tkt_id = B.ticketId
 WHERE A.ta_created_date = B.latest_assigned_date) 
 C
-ON ticket.tkt_id = C.ta_tkt_id;
+ON ticket.tkt_id = C.ta_tkt_id
+INNER JOIN status ON tkt_sts_id = sts_id
+INNER JOIN priority ON tkt_pty_id = pty_id;
+
